40d33774ab70109caa406163bf4e7edb20aaa6c7
yh_swjtu@163.com <yh_swjtu@163.com>
2018-11-18 16:05:02 +0800

    update at Nov 17th

---
+++ BasicKnowledge/cmake-build-debug/CMakeFiles/BasicKnowledge.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BasicKnowledge.dir/main.cpp.obj"
+  "BasicKnowledge.pdb"
+  "BasicKnowledge.exe"
+  "BasicKnowledge.exe.manifest"
+  "libBasicKnowledge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicKnowledge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
