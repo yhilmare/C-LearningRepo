dab9912443e7f8937dd74d8af510203c845eb2df
yh_swjtu@163.com <yh_swjtu@163.com>
2018-11-19 22:40:53 +0800

    update at Nov 19th

--- BasicKnowledge/main.cpp
+++ BasicKnowledge/main.cpp
@@ -1,17 +1,90 @@
 #include <iostream>
 #include <cmath>
 #include <climits>
+#include <string>
+#include <cstring>
 
 using std::cout;
 using std::cin;
 
 void Chapter2();
 void Chapter3();
+void Chapter4();
 
 int main() {
 //    Chapter2();
-    Chapter3();
+//    Chapter3();
+    Chapter4();
     return 0;
+}
+
+void Chapter4(){
+    using namespace std;
+    //数组
+    int array[4] = {1,2,3,5};
+    for (int i = 0; i < 4; i ++){
+        cout << array[i] << endl;
+    }
+    int a[5] = {1, 2};//若只对数组的一部分赋值，则其余部分为0
+    for (int i = 0; i < 5; i ++){
+        cout << a[i] << endl;
+    }
+    int b[] = {1, 2, 5, 6, 9, 85};
+    int count = sizeof(b) / sizeof(int);
+    for (int i = 0; i < count; i ++){
+        if (i < count - 1){
+            cout << b[i] << " - ";
+        }else{
+            cout << b[i] << endl;
+        }
+    }
+    //字符串
+    char str[] = "I love my country";
+    cout << str << endl;
+    char str1[] = {'I', ' ', 'l', 'o', 'v', 'e', ' ', 'm', 'y', ' ', 'c', 'o', 'u', 'n', 't', 'r', 'y', 0};
+    cout << str1 << endl;
+    char str2[8] = {'I', ' ', 'l', 'o', 0, 'v', 'e', ' '};
+    cout << str2 << endl;
+    /*
+     * 传统的cin输入以空格为输入分割，若要输入的一个字符串中带有空格，则不能很好地录入，
+     * 只能录入空格前的字符串，空格后的字符串会被缓存到输入队列中供后面的输入使用
+     * */
+//    const int size = 20;
+//    char name[size];
+//    char desert[size];
+//    cout << "Enter your name: " << endl;
+//    cin >> name;
+//    cout << "Enter your favourite desert:" << endl;
+//    cin >> desert;
+//    cout << "I have some delicious " << desert << " for you, " << name << endl;
+
+//    char str3[30];
+//    cin.getline(str3, 30);//一共两个参数，第一个参数说明要读入的目标字符串，第二个参数说明要读入的长度，以换行Enter为结尾，不保留换行符
+//    cout << str3 << endl;
+//    char name1[30];
+//    char name2[30];
+//    cin.getline(name1, 30).getline(name2, 30);
+//    cin.get(name1, 30).get();
+    /*
+     * 这样是由于cin.get函数不接受换行符，因此换行符保留在输入缓冲里，
+     * 当下一个输入到来时就会将这个换行符作为第一个输入从而导致错误，因此用一个cin.get在这里接住这个换行符。
+     * */
+//    cin.get(name2, 30);
+//    cout << name1 << name2 << endl;
+
+    //string类
+    string str4 = "I love my country";
+    str4 = {"I am Chinese\n"};//不同的string初始化方式
+    string str5{"I love my country"};
+    cout << str4 << " - " << str4.size() << " - " << str4.length() << endl;
+    cout << str4 + str5 << endl;
+
+    char charr[20];
+    cout << "Length of string in charr before input: "
+         << strlen(charr) << endl;
+    string str6;
+    getline(cin, str6);
+    cout << str6 << endl;
 }
 
 void Chapter3(){
@@ -48,6 +121,10 @@ void Chapter3(){
     float f = 3.0;
     cout.setf(ios_base::fixed, ios_base::floatfield);//用于强制规定浮点数的打印位数，防止把过大的浮点数打印成E表示的浮点数
     cout << e / f << endl;
+
+    int thorn = 3;
+    double thorn_d = static_cast<double>(thorn);//强制转换
+    cout << thorn_d << endl;
 }
 void Chapter2(){
     /*
