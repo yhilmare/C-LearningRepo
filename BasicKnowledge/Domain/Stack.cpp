28d3f750fcc5951e8711dbd563a833ba417a7877
yhswjtuilmare <yh_swjtu@163.com>
2018-12-10 22:53:41 +0800

    update at Dec 10th

--- BasicKnowledge/Domain/Stack.cpp
+++ BasicKnowledge/Domain/Stack.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Stack.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+Stack::Stack() {
+    stack_size = 0;
+    index = -1;
+    p_first = new Node[1];
+    p_first->p_next = NULL;
+    p_current = p_first;
+}
+int Stack::get_size() {
+    return stack_size;
+}
+
+bool Stack::push(Item &item) {
+    Node *p_new = new Node[1];
+    p_new->item = item;
+    if (p_first->p_next == NULL){
+        p_first->p_next = p_new;
+        p_new->p_next = NULL;
+        return true;
+    }
+    p_current = p_first->p_next;
+    p_first->p_next = p_new;
+    p_new->p_next = p_current;
+    return true;
+}
+
+Item Stack::pop() {
+    if (p_first->p_next == NULL){
+        return Item();
+    }
+    p_current = p_first->p_next;
+    Item result = p_current->item;
+    delete []p_current;
+    return result;
+}
+Stack::~Stack() {
+    Node *tmp;
+    for (Node *p = p_first; p; p = tmp){
+        tmp = p->p_next;
+        delete []p;
+    }
+}
+std::ostream& Stack::operator<<(std::ostream &os, const Stack &s) {
+    for (Node *p = p_first->p_next; p; p = p->p_next){
+        cout << p->item << " - ";
+    }
+}
+
--- BasicKnowledge/Domain/Stack.h
+++ BasicKnowledge/Domain/Stack.h
@@ -5,9 +5,39 @@
 #ifndef BASICKNOWLEDGE_STACK_H
 #define BASICKNOWLEDGE_STACK_H
 
+#include <string>
+class customer{
+private:
+    std::string name;
+    int age;
+public:
+    friend std::ostream &operator<<(std::ostream &os, const customer &s);
+};
+inline std::ostream &operator<<(std::ostream &os, const customer &s){
+//    os << "\{" << s.name << ", " << s.age << "\}";
+    return os;
+}
+
+typedef customer Item;
 
 class Stack {
+private:
+    struct Node{
+        Item item;
+        Node *p_next;
+    };
+    Node *p_first;
+    Node *p_current;
+    int stack_size;
+    int index;
+public:
+    Stack();//默认构造函数，作为一个栈，不应该提供默认构造函数之外的构造函数
+    ~Stack();//析构函数，释放全部链表
+    int get_size();
+    bool push(Item &item);
+    Item pop();
 
+    friend std::ostream &operator<<(std::ostream &os, const Stack &s);
 };
 
 
